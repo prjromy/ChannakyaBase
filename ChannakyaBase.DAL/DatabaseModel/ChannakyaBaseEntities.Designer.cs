f7ffc4302885c9c650d30af687650f6986897c73
prjromy <prjromy@gmail.com>
2020-01-09 10:12:00 +0545

    Update ChannakyaBaseEntities.Designer.cs

--- ChannakyaBase.DAL/DatabaseModel/ChannakyaBaseEntities.Designer.cs
+++ ChannakyaBase.DAL/DatabaseModel/ChannakyaBaseEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\WorkspaceAll\Channakya Base\Channakya Base\ChannakyaBase.DAL\DatabaseModel\ChannakyaBaseEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\BACKUPS 2019 christmas\Channakya Base\Channakya Base\ChannakyaBase.DAL\DatabaseModel\ChannakyaBaseEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
